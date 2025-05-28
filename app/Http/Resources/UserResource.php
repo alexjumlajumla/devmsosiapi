2d8d2e16d24273303f6e95c4154a62d8be3ca820
Alex Jumla <alex@jumlajumla.com>
2025-05-29 01:11:42 +0300

    UserResource fix date fields to handle both string and carbon instance

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -55,15 +55,39 @@ class UserResource extends JsonResource
             'referral'              => $this->when($this->referral, $this->referral),
             'my_referral'           => $this->when($this->my_referral, $this->my_referral),
             'role'                  => $this->when($this->role, $this->role),
-            'email_verified_at'     => $this->when($this->email_verified_at,
-                $this->email_verified_at?->format('Y-m-d H:i:s') . 'Z'
-            ),
-            'phone_verified_at'     => $this->when($this->phone_verified_at,
-                $this->phone_verified_at?->format('Y-m-d H:i:s') . 'Z'
-            ),
-            'registered_at'         => $this->when($this->created_at,
-                $this->created_at?->format('Y-m-d H:i:s') . 'Z'
-            ),
+            'email_verified_at'     => $this->when($this->email_verified_at, function() {
+                $date = $this->email_verified_at;
+                if (is_string($date)) {
+                    try {
+                        $date = \Carbon\Carbon::parse($date);
+                    } catch (\Exception $e) {
+                        return $date;
+                    }
+                }
+                return $date ? $date->format('Y-m-d H:i:s') . 'Z' : null;
+            }),
+            'phone_verified_at'     => $this->when($this->phone_verified_at, function() {
+                $date = $this->phone_verified_at;
+                if (is_string($date)) {
+                    try {
+                        $date = \Carbon\Carbon::parse($date);
+                    } catch (\Exception $e) {
+                        return $date;
+                    }
+                }
+                return $date ? $date->format('Y-m-d H:i:s') . 'Z' : null;
+            }),
+            'registered_at'         => $this->when($this->created_at, function() {
+                $date = $this->created_at;
+                if (is_string($date)) {
+                    try {
+                        $date = \Carbon\Carbon::parse($date);
+                    } catch (\Exception $e) {
+                        return $date;
+                    }
+                }
+                return $date ? $date->format('Y-m-d H:i:s') . 'Z' : null;
+            }),
             'orders_sum_price'      => $this->when($this->orders_sum_total_price, $this->orders_sum_total_price),
             'delivery_man_orders_count' => $this->when($this->delivery_man_orders_count, $this->delivery_man_orders_count),
             'delivery_man_orders_sum_total_price' => $this->when($this->delivery_man_orders_sum_total_price,
@@ -75,9 +99,39 @@ class UserResource extends JsonResource
                 $this->assign_reviews_avg_rating
             ),
             'review_count_by_rating'        => $this->when($reviewsGroup, $reviewsGroup),
-            'created_at'                    => $this->when($this->created_at, $this->created_at?->format('Y-m-d H:i:s') . 'Z'),
-            'updated_at'                    => $this->when($this->updated_at, $this->updated_at?->format('Y-m-d H:i:s') . 'Z'),
-            'deleted_at'                    => $this->when($this->deleted_at, $this->deleted_at?->format('Y-m-d H:i:s') . 'Z'),
+            'created_at'                    => $this->when($this->created_at, function() {
+                $date = $this->created_at;
+                if (is_string($date)) {
+                    try {
+                        $date = \Carbon\Carbon::parse($date);
+                    } catch (\Exception $e) {
+                        return $date;
+                    }
+                }
+                return $date ? $date->format('Y-m-d H:i:s') . 'Z' : null;
+            }),
+            'updated_at'                    => $this->when($this->updated_at, function() {
+                $date = $this->updated_at;
+                if (is_string($date)) {
+                    try {
+                        $date = \Carbon\Carbon::parse($date);
+                    } catch (\Exception $e) {
+                        return $date;
+                    }
+                }
+                return $date ? $date->format('Y-m-d H:i:s') . 'Z' : null;
+            }),
+            'deleted_at'                    => $this->when($this->deleted_at, function() {
+                $date = $this->deleted_at;
+                if (is_string($date)) {
+                    try {
+                        $date = \Carbon\Carbon::parse($date);
+                    } catch (\Exception $e) {
+                        return $date;
+                    }
+                }
+                return $date ? $date->format('Y-m-d H:i:s') . 'Z' : null;
+            }),
             'referral_from_topup_price'     => $this->when(request('referral'), $this->referral_from_topup_price),
             'referral_from_withdraw_price'  => $this->when(request('referral'), $this->referral_from_withdraw_price),
             'referral_to_withdraw_price'    => $this->when(request('referral'), $this->referral_to_withdraw_price),
